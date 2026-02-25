7fc279aa050ec011fd8311ac002ae165d937a406
tanndlin <tanndlin@gmail.com>
2026-02-24 23:00:29 -0500

    Add support for images

--- src/types.rs
+++ src/types.rs
@@ -7,11 +7,21 @@ pub struct Header {
 
 #[derive(PartialEq, Eq)]
 #[allow(clippy::upper_case_acronyms)]
+pub enum ImageType {
+    PNG,
+    JPEG,
+    GIF,
+}
+
+#[derive(PartialEq, Eq)]
+#[allow(clippy::upper_case_acronyms)]
 pub enum ContentType {
     HTML,
     Text,
     JavaScript,
     CSS,
+    JSON,
+    Image(ImageType),
     Unknown,
 }
 
@@ -22,6 +32,10 @@ impl ContentType {
             "txt" => ContentType::Text,
             "js" => ContentType::JavaScript,
             "css" => ContentType::CSS,
+            "json" => ContentType::JSON,
+            "png" => ContentType::Image(ImageType::PNG),
+            "jpg" | "jpeg" => ContentType::Image(ImageType::JPEG),
+            "gif" => ContentType::Image(ImageType::GIF),
             _ => ContentType::Unknown,
         }
     }
@@ -40,6 +54,10 @@ impl Display for ContentType {
             ContentType::Text => "text/plain",
             ContentType::JavaScript => "application/javascript",
             ContentType::CSS => "text/css",
+            ContentType::JSON => "application/json",
+            ContentType::Image(ImageType::PNG) => "image/png",
+            ContentType::Image(ImageType::JPEG) => "image/jpeg",
+            ContentType::Image(ImageType::GIF) => "image/gif",
             ContentType::Unknown => "application/octet-stream",
         };
         write!(f, "{}", content_type_str)
